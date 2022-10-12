0144562584f3a2cee545d59049f792e96869e03a
jbull328 <jackbull328@gmail.com>
2022-10-12 09:38:55 -0700

    chore: save progress

---
+++ src/components/UsersList.vue
@@ -0,0 +1,24 @@
+<script setup>
+    import defineProps from 'vue'
+
+    const props = defineProps({
+        users: Object
+    });
+    
+</script>
+
+<template>
+    <b-card v-for="user in users" :key="user"
+        :title="user.name"
+        :img-src="user.avatar"
+        img-alt="Image"
+        img-top
+        tag="user"
+        style="max-width: 20rem;"
+        class="mb-2"
+    >
+
+
+    </b-card>
+    {{users}}
+</template>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,33 @@
-<script>
-    export default {
+<script setup>
+    import { reactive } from 'vue';
+    import axios from 'axios';
 
-    }
+    const users = reactive('')
+    const comments = reactive('')
+        
+    const getData = async function() {
+        await axios.get('assets/users.json').then(response => (
+            users.value = response
+        ));
+        await axios.get('assets/comments.json').then(response => (
+            comments.value = response
+        )); 
+    };
+
+    getData();
+
 </script>
 
 <template>
-    <div>
+    <div class="container">
         <b-tabs content-class="mt-3">
-            <b-tab title="Users" active><p>This will be users</p></b-tab>
+            <b-tab title="Users" active><UsersList :users="users"/></b-tab>
             <b-tab title="Comments" ><p>This will be Comments</p></b-tab>
         </b-tabs>
+        Why {{users}}
     </div>
 </template>
+
+<style>
+
+</style>
