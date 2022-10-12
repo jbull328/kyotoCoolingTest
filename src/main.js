290022df1f472f09ef8a517583631311c3ea6630
jbull328 <jackbull328@gmail.com>
2022-10-12 08:43:23 -0700

    chore: add bootstrap3

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+//BootstrapVue3 is experamental and on production ready:
+//https://cdmoro.github.io/bootstrap-vue-3/getting-started/
+import BootstrapVue3 from 'bootstrap-vue-3'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+const app = createApp(App)
+app.use(BootstrapVue3)
+app.mount('#app')
---
+++ src/pages/Index.vue
@@ -0,0 +1,14 @@
+<script>
+    export default {
+
+    }
+</script>
+
+<template>
+    <div>
+        <b-tabs content-class="mt-3">
+            <b-tab title="Users" active><p>This will be users</p></b-tab>
+            <b-tab title="Comments" ><p>This will be Comments</p></b-tab>
+        </b-tabs>
+    </div>
+</template>
